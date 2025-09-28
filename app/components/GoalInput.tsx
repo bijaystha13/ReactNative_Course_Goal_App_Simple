3f1eed5f9c7fc73e1d76ebc8ec1a8281d207af3a
BIJAY SHRESHTHA <shresthabzay95@gmail.com>
2025-09-27 22:20:37 -0400

    Modal Added

---
+++ app/components/GoalInput.tsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import { Button, Modal, StyleSheet, TextInput, View } from "react-native";
+
+type GoalInputPros = {
+  onAddGoal: (goalText: string) => void;
+  visible: boolean;
+};
+
+export default function GoalInput({ onAddGoal, visible }: GoalInputPros) {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+
+  function goalInputHandler(enteredText: string) {
+    setEnteredGoalText(enteredText);
+  }
+
+  function addGoalHandler() {
+    onAddGoal(enteredGoalText);
+    setEnteredGoalText("");
+  }
+
+  return (
+    <Modal visible={visible} animationType="slide">
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your Course Goal!"
+          onChangeText={goalInputHandler}
+          value={enteredGoalText}
+        />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button title="Add Goal" onPress={addGoalHandler} />
+          </View>
+          <View style={styles.button}>
+            <Button title="Cancel" onPress={addGoalHandler} />
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
+}
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 24,
+    padding: 16,
+    borderBottomWidth: 1,
+    borderColor: "#cccccc",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "100%",
+    padding: 8,
+  },
+  buttonContainer: {
+    marginTop: 16,
+    flexDirection: "row",
+  },
+  button: {
+    width: 100,
+    marginHorizontal: 8,
+  },
+});
---
+++ app/components/GoalItem.tsx
@@ -0,0 +1,36 @@
+import { Pressable, StyleSheet, Text, View } from "react-native";
+
+type GoalItemProps = {
+  id: any;
+  text: string;
+  onDeleteItem: (goalText: string) => void;
+};
+
+export default function GoalItem({ text, onDeleteItem, id }: GoalItemProps) {
+  return (
+    <View style={styles.goalItem}>
+      <Pressable
+        android_ripple={{ color: "#210644" }}
+        onPress={() => onDeleteItem(id)}
+        style={({ pressed }) => pressed && styles.pressedItem}
+      >
+        <Text style={styles.goalText}>{text}</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  goalItem: {
+    margin: 8,
+    borderRadius: 6,
+    backgroundColor: "#5e0acc",
+  },
+  pressedItem: {
+    opacity: 0.5,
+  },
+  goalText: {
+    padding: 8,
+    color: "white",
+  },
+});
